a442170b8a6a79a4fa3a12bd6867c8f8bc17d239
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2022-08-27 11:17:02 +0530

    Hello! Mom

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,28 +31,6 @@ interface Props {
   posts: Post[];
 }
 
-const WavingHand = () => (
-  <motion.div
-    style={{
-      marginBottom: '-20px',
-      marginRight: '-45px',
-      paddingBottom: '20px',
-      paddingRight: '45px',
-      display: 'inline-block',
-    }}
-    animate={{ rotate: 20 }}
-    transition={{
-      repeat: 7,
-      repeatType: 'mirror',
-      duration: 0.2,
-      delay: 0.5,
-      ease: 'easeInOut',
-      type: 'tween',
-    }}
-  >
-    👋🏻
-  </motion.div>
-);
 
 let year = 0;
 
