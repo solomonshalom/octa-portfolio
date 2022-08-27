a0a3d7c783dcd04be0c4d9656bb3369aeceee265
Solomon lijo <71135230+solomonshalom@users.noreply.github.com>
2022-08-27 15:28:45 +0530

    Hello! Mom

--- layouts/Signature/Signature.tsx
+++ layouts/Signature/Signature.tsx
@@ -24,7 +24,7 @@ const ColoredBlockWrapper = styled('div', {
   },
 });
 
-const Signature = ({ title, url }: { title: string; url: string }) => {
+const Signature = () => {
   return (
     <ColoredBlockWrapper data-testid="signature">
       <section>
